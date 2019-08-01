c353b1ca4ceaa5dd4365593ec7fab99a6ccd0406
Brady Hislop <hislopbrady@gmail.com>
2019-08-01 11:45:05 -0700

    Trying to open file with roo

--- backend/app/controllers/spree/admin/general_settings_controller.rb
+++ backend/app/controllers/spree/admin/general_settings_controller.rb
@@ -65,7 +65,7 @@ module Spree
         logger.debug "********** Errors: #{@excel.errors.full_messages}"
 
         if @excel.save
-            #xlsx = Roo::Spreadsheet.open(path, 'r')
+            xlsx = Roo::Spreadsheet.open(path, 'r')
             #open_part = Spreadsheet.open(path)
             #part = open_part.worksheet(0)
            #skip the first column of each row.
