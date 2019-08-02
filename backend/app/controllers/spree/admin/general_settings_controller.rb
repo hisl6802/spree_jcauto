586e80f704b67f4e659253c097930be8677343b5
Brady Hislop <hislopbrady@gmail.com>
2019-08-02 15:32:47 -0700

    Syntax error fix

--- backend/app/controllers/spree/admin/general_settings_controller.rb
+++ backend/app/controllers/spree/admin/general_settings_controller.rb
@@ -66,7 +66,8 @@ module Spree
         #@path = Excel.find(params[:file])
         if @excel.save
             #xlsx = Roo::Excelx.new(File.join(Rails.root,'public','spree','excels',@excel_file,'original',@excel_name))
-
+            s3 = AWS::S3::S3Object
+            
             #open_part = Spreadsheet.open(path, 'r')
             #part = open_part.worksheet(0)
            #skip the first column of each row.
